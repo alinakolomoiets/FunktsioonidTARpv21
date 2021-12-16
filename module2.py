5cb449cae46dc4dc9d11daa1e450f3efd0ac6340
Alina <93324365+alinakolomoiets@users.noreply.github.com>
2021-12-16 15:22:23 +0200

    Update module2.py

--- module2.py
+++ module2.py
@@ -33,3 +33,63 @@ def Liigaasta(aasta:int)->bool:
 	:rtype:bool
 	"""
 	if aasta
+from math import*
+#def ruud(a:float):
+#	"""
+#	:rtype:float,float,float
+#	"""
+#	S=2**a
+#	P=(a+b)*2
+#	d=sqrt(2*a*a)
+#	return P,S,d
+#Ümbermõõt,Pinadala,Diogonaal=ruud(3)
+
+def season(kuu:int)->str:
+	"""Võtme kuu number ja tagastame tekst :Talv,Kevad, Suvi või Sügis.
+	:param int:seasons
+	"""
+	#if a==1 or a==12 or a==2:
+	#	print("talv")
+	#elif a==3 or a==4 or a==5:
+	#	print("kevad")
+	#elif a==6 or a==7 or a==8:
+	#	print("suvi")
+	#elif a==9 or a==10 or a==11:
+	#	print(sügis)
+	while 1:
+		if kuu >=1 and kuu <=12:
+			if kuu in [1,2,12]:
+				s="Talv"
+			elif kuu in [3,4,5]:
+				s="Kevad"
+			elif kuu in [6,7,8]:
+				s="Suvi"
+			elif kuu in [9,10,11]:
+					s="Sügis"
+			break
+		else:
+			kuu=int(input("Kui:1-12"))
+	return s	
+def bank(a:float,years:int)->float:
+	"""
+	:param float a:Money
+	:param str years:Years
+	"""
+	for i in range(years):
+		a*=1.1
+	return a 
+#TASK6
+def is_prime(arv:int)->bool:
+	"""
+	:param int a :arv
+	"""
+	n=0#число
+	if arv<1000 or arv>0:
+		for i in range(1,arv+1):
+			arv%i==0:
+				n+=1
+	if n==2:
+		n=True 
+	else :
+		n=False
+	return n
